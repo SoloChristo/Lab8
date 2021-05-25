1f92bbb80e3aea5d0fa23c1de48bd6366599ff73
SoloChristo <acsmith@ucsd.edu>
2021-05-24 22:09:05 -0700

    add state checker

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,15 @@
  * @jest-environment jsdom
  */
 
+import { pushToHistory } from "../scripts/router";
+describe('my history', () => {
+    test('length should be the same', () => {
+        pushToHistory("lmao", 5);
+        pushToHistory("lmao2", 7);
+        expect(window.history.length).toBe(3);
+    });
+
+    test('Current states should match', () => {
+        expect(hist.state).toBe("lmao2");
+    });
+});
